6c0a6b497f5c53f4fb0d9a20b39a3d211c0a7281
wook <TMQOFSJA@NAVER.COMm>
2024-06-12 12:39:16 +0900

    ,nm,

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "main.h"
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+
+  pinInit();
+  lcdInit();
+
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+
+#include <Arduino.h>
+
+
+#include "peripherals/peripherals.h"
+#include "lcd/lcd.h"
+
+
